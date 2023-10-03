3c3431d12365cb0d6ccb9f79222c335d4521ee8b
aydin :^) <a219201085@unison.mx>
2023-10-03 11:22:08 -0700

    27 - ui update

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -6,39 +6,63 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUpTime = Date.now
+    @State private var wakeUpTime = defaultWakeUpTime
     @State private var coffeeAmount = 1
-    /*
-    var components = DateComponents()
-    components.hour = 8
-    components.minute = 0
-    let date = Calendar.current.date(from: components) ?? Date.now
+    @State private var alertTitle = ""
+    @State private var alertText = ""
+    @State private var showingAlert = false
     
-    let components2 = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-    let hour = components2.hour ?? 0
-    let minutes = components2.minute ?? 0
-    */
+    // static allows us to read it bc it doesn't rely on any other property
+    // belongs to the struct itself rather than an instance
+    static var defaultWakeUpTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     func calculateBedtime(){
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUpTime)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Int64(Double(hour+minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            let sleepTime = wakeUpTime - prediction.actualSleep
+            alertTitle = "your ideal bedtime is..."
+            alertText = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "error"
+            alertText = "something went wrong ://"
+        }
+        showingAlert = true
     }
     
     var body: some View {
         NavigationView{
-            VStack {
-                Text("when do u want to wake up?")
-                    .font(.headline)
-                DatePicker("please enter a time", selection: $wakeUpTime, in: Date.now..., displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                Text("desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hrs", value: $sleepAmount, in: 4...12, step: 0.25)
-                Text("daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            Form {
+                VStack(alignment: .leading) {
+                    Text("when do u want to wake up?")
+                        .font(.headline)
+                    DatePicker("please enter a time", selection: $wakeUpTime, in: Date.now..., displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                VStack(alignment: .leading) {
+                    Text("desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hrs", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                VStack(alignment: .leading) {
+                    Text("daily coffee intake")
+                        .font(.headline)
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
                 /*
                  Text(Date.now, format: .dateTime.hour().minute())
                  Text(Date.now, format: .dateTime.day().month().year())
@@ -49,7 +73,11 @@ struct ContentView: View {
             .toolbar {
                 Button("calculate", action: calculateBedtime)
             }
-            .padding()
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("ok"){}
+            } message: {
+                Text(alertText)
+            }
         }
     }
 }
