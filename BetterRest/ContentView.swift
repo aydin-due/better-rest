9629f530df4bf24c5b8e915c122461cfdbd72794
aydin :^) <a219201085@unison.mx>
2023-10-03 10:50:23 -0700

    27 - layout

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUpTime = Date.now
+    @State private var coffeeAmount = 1
     /*
     var components = DateComponents()
     components.hour = 8
@@ -20,16 +21,36 @@ struct ContentView: View {
     let hour = components2.hour ?? 0
     let minutes = components2.minute ?? 0
     */
+    
+    func calculateBedtime(){
+        
+    }
+    
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hrs", value: $sleepAmount, in: 4...12, step: 0.25)
-            DatePicker("enter a date", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
-                .labelsHidden()
-            Text(Date.now, format: .dateTime.hour().minute())
-            Text(Date.now, format: .dateTime.day().month().year())
-            Text(Date.now.formatted(date: .long, time: .shortened))
+        NavigationView{
+            VStack {
+                Text("when do u want to wake up?")
+                    .font(.headline)
+                DatePicker("please enter a time", selection: $wakeUpTime, in: Date.now..., displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hrs", value: $sleepAmount, in: 4...12, step: 0.25)
+                Text("daily coffee intake")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                /*
+                 Text(Date.now, format: .dateTime.hour().minute())
+                 Text(Date.now, format: .dateTime.day().month().year())
+                 Text(Date.now.formatted(date: .long, time: .shortened))
+                 */
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("calculate", action: calculateBedtime)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
